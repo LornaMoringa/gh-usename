82456cefaf83da11e3627f5aca6e08f34c0989f4
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-05-24 09:32:41 +0300

    generated git-hub component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { GitHubComponent } from './git-hub/git-hub.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    GitHubComponent
   ],
   imports: [
     BrowserModule,
